fec726bc721dc65a0e7e33129b5805011219819d
TomasIanchina3 <tomas.ianchina@alumnos.frm.utn.edu.ar>
2023-04-28 12:26:28 -0300

    Version 1.1.2 Metodos de archivos

---
+++ PythonProject/Archivo_No_Encontrado.py
@@ -0,0 +1,10 @@
+from os import open
+
+nombre = 'archivoNoExiste.txt'
+try:
+with open(nombre) as objt_a:
+    contenido = objt_a.read()
+except FileNotFoundError:
+mensaje="ERROR Archivo no encotrado"
+print(mensaje)
+
