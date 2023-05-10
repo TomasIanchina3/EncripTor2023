8f471bd346964bf15890b5c1359ea3a3f90f2db1
JenaroManinoMartin <jenaro.manino@alumnos.frm.utn.edu.ar>
2023-05-10 18:28:15 -0300

    Modi Ventana

--- PythonProject/Ventana.py
+++ PythonProject/Ventana.py
@@ -2,11 +2,11 @@ import tkinter as tk
 import Encriptado
 import Desencriptado
 
-#Creacion ventana
+# Creacion ventana
 ventana = tk.Tk()
 ventana.title("EncripTor 2023")
 ventana.config(width=400, height=400)
-#Configuracion de menu
+# Configuracion de menu
 menu_prin = tk.Menu()
 menu_arch = tk.Menu(menu_prin, tearoff=0)
 menu_prin.add_cascade(label="Archivo", menu=menu_arch)
@@ -18,10 +18,10 @@ menu_arch.add_command(label="Guardar como...")
 menu_prin.add_cascade(label="Acerca de...")
 menu_prin.add_command(label="Salir", command=ventana.destroy)
 ventana.config(menu=menu_prin)
-#Configuracion botones
+# Configuracion botones
 boton_encriptar=tk.Button(text="ENCRIPTAR", padx=15, pady=15, command= lambda: Encriptado.encriptar())
 boton_encriptar.place(x=150, y=100)
 boton_desencriptar=tk.Button(text="DESENCRIPTAR", padx=15, pady=15, command= lambda: Desencriptado.desencriptar())
 boton_desencriptar.place(x=150, y=200)
 
-ventana.mainloop()
+# ventana.mainloop()
