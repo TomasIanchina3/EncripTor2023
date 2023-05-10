bddd8b6951e3c24611f31a55ba5d130bd50b36a6
TomasIanchina3 <tomas.ianchina@alumnos.frm.utn.edu.ar>
2023-05-10 18:15:24 -0300

    1.1.6fix

--- PythonProject/Encriptado.py
+++ PythonProject/Encriptado.py
@@ -6,44 +6,36 @@ from tkinter  import filedialog
 
 from more_itertools import run_length
 
-#Primero vamos a generar el numero random entre 1 y 10
-def encriptar():
+#Primero vamos a generar el numero random entre 1 y 4
+llave = random.randint(1,4)
 
-    llave = random.randint(1,10)
+ventana = tk.Tk()
 
-    ventana = tk.Tk()
+ruta_archivo = filedialog.askopenfilename()
 
-    ruta_archivo = filedialog.askopenfilename()
+print(ruta_archivo)
+with open(ruta_archivo) as ar_object:
+    texto = ar_object.read()
 
-    print(ruta_archivo)
-    with open(ruta_archivo) as ar_object:
-        texto = ar_object.read()
+#Una vez leido el archivo en la cadena
+#Lo remplazamos con el encriptado con un for que recorrera esa cadena
+encriptado = ""
+#Chicos realice modificaciones este codigo
+#Al ser la version beta este solo admitira el codigo ASCII pero no el codigo ASCII extendido ya que deberiamos aplicar UNICODE derivando en otro metodo distinto
+for caracter in texto:
+    codigoa = ord(caracter)
+    if codigoa >= 32 and codigoa <= 126:
+      ce = codigoa + llave
+      caracterencriptado = chr(ce)
+      encriptado += caracterencriptado
+    else:
+        encriptado += caracter
 
-    #Una vez leido el archivo en la cadena
-    #Lo remplazamos con el encriptado con un for que recorrera esa cadena
-    encriptado = ""
-    #Chicos realice modificaciones este codigo
-    #Al ser la version beta este solo admitira el codigo ASCII pero no el codigo ASCII extendido ya que deberiamos aplicar UNICODE derivando en otro metodo distinto
-    for caracter in texto:
-        encriptado.append(str(ord(caracter) + llave))
+#Ahora procederemos a modificar nuestro archivo de texto base
 
-    #Para desencriptarlo necesitamos la funcion char("Valor entero")
-    texto2= ""
-    for caracter in encriptado:
-        texto2 +=chr(int(caracter))
-        codigoa = ord(caracter)
-        if codigoa >= 32 and codigoa <= 126:
-          ce = codigoa + llave
-          caracterencriptado = chr(ce)
-          encriptado += caracterencriptado
-        else:
-            encriptado += caracter
+with open(ruta_archivo,'w') as ar_object:
+    ar_object.write(str(llave))
 
-    #Ahora procederemos a modificar nuestro archivo de texto base
-
-    with open(ruta_archivo,'w') as ar_object:
-        ar_object.write(str(llave))
-
-    with open(ruta_archivo,'a') as ar_object:
-        ar_object.write(encriptado)
-       #Para Este resultado se uso el archivo prueba para crear
+with open(ruta_archivo,'a') as ar_object:
+    ar_object.write(encriptado)
+   #Para Este resultado se uso el archivo prueba para crear
