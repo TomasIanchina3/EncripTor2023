1a7b25f9c02dfa5eb714bd0c533094e616023b11
JenaroManinoMartin <jenaro.manino@alumnos.frm.utn.edu.ar>
2023-06-14 15:10:33 -0300

    Emprolijar código

--- PythonProject/Lector Archivo.py
+++
@@ -1,8 +0,0 @@
-#El metodo de aca abajo es para abrir,leer y mostrar el archivo
-#Se utiliza el metodo with para cerrar automaticamente el archivo
-#Pero se debe crear como un objeto el contenido del mismo como se ve mas abajo
-with open('./prueba_git.txt') as objeto_Archivo:
-    contenido = objeto_Archivo.read()
-    print(contenido)
-
-
--- PythonProject/Linea_a_linea.py
+++
@@ -1,8 +0,0 @@
-nombre_Archivo = './prueba_git.txt'
-with open(nombre_Archivo) as objecto_A:
-    lineas = objecto_A.readlines()
-
- for line in lineas:
-     print(line.rstrip())
-
-#Este metodo funciona para leer el archivo linea por linea
